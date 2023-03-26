22e9cf56f12f7cd40f761f4de4ca18bc310ca3e6
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2023-03-27 00:14:13 +0530

    Update video_hentai.py

--- plugin/video_hentai.py
+++ plugin/video_hentai.py
@@ -7,8 +7,9 @@ import subprocess
 import json
 
 MONGO_URL = os.environ.get("MONGO_URL", None) 
-CACHE_CHANNEL = os.environ.get(int("CACHE_CHANNEL"))
-
+CACHE_CHANNEL = os.environ.get("CACHE_CHANNEL")
+print(CACHE_CHANNEL)
+CACHE_CHANNEL = int(CACHE_CHANNEL)
 def hentailink(client, callback_query):
     click = callback_query.data
     clickSplit = click.split("_")
